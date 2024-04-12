edb3bad9eb7ef4057183079495999a54a8b7ddf2
Prathameshj-git <prathameshj2502@gmail.com>
2024-04-12 14:33:29 +0530

    commit

---
+++ cod task 2/script.js
@@ -0,0 +1,24 @@
+document.querySelector('.cross').style.display='none';
+document.querySelector('.hamberg').addEventListener("click",()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+
+
+    if(document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display='inline';
+
+        
+            document.querySelector('.cross').style.display='none';
+ 
+        
+      
+    }
+    else{
+        
+
+        setTimeout(() => {
+            document.querySelector('.cross').style.display='inline'; 
+        }, 300);
+        document.querySelector('.ham').style.display='none';
+    }
+
+})
